edadea2961976c9002a3021e413a49f7b9a4b872
Guido Pascariello <guidop@kozaca.com.ar>
2020-12-20 12:31:28 -0300

    ajustes urls

--- rest/plantas/urls.py
+++ rest/plantas/urls.py
@@ -4,7 +4,7 @@ from plantas import views
 
 urlpatterns = [
     
-    path('lista', views.PlantaViewSet.as_view({'get': 'list', 'post': 'create'}), name='plantas-list'),
-    path('<int:pk>', views.PlantaViewSet.as_view({'get': 'retrieve','put': 'update','delete': 'destroy'}), name='plantas-list'),
+    path('s', views.PlantaViewSet.as_view({'get': 'list', 'post': 'create'}), name='plantas-list'),
+    path('/<int:pk>', views.PlantaViewSet.as_view({'get': 'retrieve','put': 'update','delete': 'destroy'}), name='plantas-list'),
 
 ]
--- rest/rest/urls.py
+++ rest/rest/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('plantas/', include('plantas.urls')),
+    path('admin', admin.site.urls),
+    path('planta', include('plantas.urls')),
 ]
