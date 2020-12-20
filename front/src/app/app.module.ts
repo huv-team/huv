58341c5841ea8d9f5123b4c961383c04e0427acb
Guido Pascariello <guidop@kozaca.com.ar>
2020-12-20 17:49:36 -0300

    atoms module

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ComponentsModule } from "./components/components.module";
+
 @NgModule({
   declarations: [
     AppComponent
@@ -14,6 +16,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    ComponentsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
