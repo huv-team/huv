a9c0b5ba56e117092142c7d5cedd7d099198239c
Ramiro Gatti <rgatti@ingenieria.uner.edu.ar>
2020-12-28 15:56:40 -0300

    Nombre app

--- rest/plantas/urls.py
+++ rest/plantas/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 
 from plantas import views
 
+app_name = 'plantas'
 urlpatterns = [
-    
-    path('s', views.PlantaViewSet.as_view({'get': 'list', 'post': 'create'}), name='plantas-list'),
-    path('/<int:pk>', views.PlantaViewSet.as_view({'get': 'retrieve','put': 'update','delete': 'destroy'}), name='plantas-list'),
+
+    path('s', views.PlantaViewSet.as_view({'get': 'list', 'post': 'create'}),
+         name='plantas-list'),
+    path('<int:pk>', views.PlantaViewSet.as_view({'get': 'retrieve',
+         'put': 'update', 'delete': 'destroy'}), name='plantas-list'),
 
 ]
