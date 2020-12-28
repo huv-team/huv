3ce019596e6f722ffaeb4ddf253b9268c79de6e3
Ramiro Gatti <rgatti@ingenieria.uner.edu.ar>
2020-12-28 15:56:58 -0300

    Estilo

--- rest/plantas/views.py
+++ rest/plantas/views.py
@@ -3,20 +3,17 @@ from rest_framework.response import Response
 
 from django.db.models import Q
 
-from plantas import models,serializer
+from plantas import models, serializer
+
 
 class PlantaViewSet(viewsets.ModelViewSet):
     serializer_class = serializer.PlantaSerializer
 
     def get_queryset(self,):
-        
         query = Q()
-        
         nombre = self.request.query_params.get('nombre', None)
-        if nombre is not None: query &= Q(
-            Q(nombre_cientifico__icontains=nombre) | Q(nombre_popular__icontains=nombre)
-        )
-        
+        if nombre is not None:
+            query &= Q(Q(nombre_cientifico__icontains=nombre) |
+                       Q(nombre_popular__icontains=nombre))
         queryset = models.Planta.objects.filter(query)
         return queryset
-
