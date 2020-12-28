da4607e7ea669aa38bd3c3f4a788b706952a0e97
Ramiro Gatti <rgatti@ingenieria.uner.edu.ar>
2020-12-28 15:57:59 -0300

    Nombre en funcion e interacciones

--- rest/plantas/models.py
+++ rest/plantas/models.py
@@ -7,62 +7,64 @@ MESES = [('ENE', 'Enero'), ('FEB', 'Febrero'), ('MAR', 'Marzo'),
          ('OCT', 'Octubre'), ('NOV', 'Noviembre'), ('DIC', 'Diciembre')]
 
 
+def get_name(self,):
+    return self.nombre_popular if self.nombre_popular\
+                               else self.nombre_cientifico
+
+
 class Familia(models.Model):
-    nombre_cientifico = models.CharField(max_length=200, null=False)
-    nombre_popular = models.CharField(max_length=200, null=False)
-    
+    nombre_cientifico = models.CharField(max_length=200, null=True, blank=True)
+    nombre_popular = models.CharField(max_length=200, null=True, blank=True)
 
     def __str__(self,):
-        return self.nombre_popular if self.nombre_popular else self.nombre_cientifico
+        return get_name(self)
 
 
 class Tipo(models.Model):
-    nombre = models.CharField(max_length=200, null=False, choices=[
-        ('FR','Fruta'),
-        ('FL','Flor'),
-        ('HO','Hoja'),
-        ('RA','Raiz'),
+    nombre = models.CharField(max_length=200, null=True, blank=True, choices=[
+        ('FR', 'Fruta'),
+        ('FL', 'Flor'),
+        ('HO', 'Hoja'),
+        ('RA', 'Raiz'),
     ])
-
 
     def __str__(self,):
         return self.nombre
 
 
 class Planta(models.Model):
-    nombre_cientifico = models.CharField(max_length=200, null=True)
-    nombre_popular = models.CharField(max_length=200, null=True)
-    variedad = models.CharField(max_length=200, null=True)
+    nombre_cientifico = models.CharField(max_length=200, null=True, blank=True)
+    nombre_popular = models.CharField(max_length=200, null=True, blank=True)
+    variedad = models.CharField(max_length=200, null=True, blank=True)
     familia = models.ForeignKey(Familia, on_delete=models.SET_NULL, null=True,
                                 related_name='plantas')
-    tipo = models.ForeignKey(Tipo, on_delete=models.SET_NULL, null=True, related_name='fichas')
+    tipo = models.ForeignKey(Tipo, on_delete=models.SET_NULL, null=True,
+                             related_name='fichas')
 
     def __str__(self,):
-        return self.nombre_popular if self.nombre_popular else self.nombre_cientifico
+        return get_name(self)
 
 
 class Rotaciones(models.Model):
     anterior = models.ForeignKey(Familia, on_delete=models.SET_NULL, null=True,
                                  related_name='anterior')
-    actual =  models.ForeignKey(Familia, on_delete=models.SET_NULL, null=True,
-                                related_name='actual')
+    actual = models.ForeignKey(Familia, on_delete=models.SET_NULL, null=True,
+                               related_name='actual')
     posterior = models.ForeignKey(Familia, on_delete=models.SET_NULL,
                                   null=True, related_name='posterior')
 
-
     def __str__(self,):
-        return self.actual.nombre_popular if self.actual.nombre_popular else self.actual.nombre_cientifico
+        return get_name(self.actual)
 
 
 class Epoca(models.Model):
-    tipo = models.CharField(max_length=200, null=False, default='SI',
-    choices=[('SE', 'Semillero'), ('SI', 'Siembra'),
-             ('TR', 'Trasplante'), ('CO', 'Cosecha')])
-    desde = models.CharField(max_length=200, null=False, default='ENE',
-                             choices=MESES)
-    hasta = models.CharField(max_length=200, null=False, default='DIC',
-                             choices=MESES)
-
+    tipo = models.CharField(max_length=200, null=True, default='SI', 
+                            choices=[('SE', 'Semillero'), ('SI', 'Siembra'),
+                                     ('TR', 'Trasplante'), ('CO', 'Cosecha')])
+    desde = models.CharField(max_length=200, null=True, blank=True,
+                             default='ENE', choices=MESES)
+    hasta = models.CharField(max_length=200, null=True, blank=True, 
+                             default='DIC', choices=MESES)
 
     def __str__(self,):
         return '{} de {} a {}'.format(self.tipo, self.desde, self.hasta)
@@ -71,84 +73,92 @@ class Epoca(models.Model):
 class Fuente(models.Model):
     autor = models.CharField(max_length=200, null=True)
     cita = models.TextField(null=False)
-    url = models.TextField(null=True) # posibilidad de no agregar
-
+    url = models.TextField(null=True)  # posibilidad de no agregar
 
     def __str__(self,):
         return self.autor if self.autor else 'None'
 
 
 class Tip(models.Model):
-    descripcion = models.CharField(max_length=200, null=True)
-    contenido = models.TextField(null=False)
-    fuente = models.ForeignKey(Fuente, on_delete=models.SET_NULL, null=True, related_name='citada_en_tips')
-
+    descripcion = models.CharField(max_length=200, null=True, blank=True)
+    contenido = models.TextField(null=True, blank=True)
+    fuente = models.ForeignKey(Fuente, on_delete=models.SET_NULL, null=True,
+                               related_name='citada_en_tips')
 
     def __str__(self,):
         return self.descripcion if self.descripcion else 'None'
 
 
 class Sustrato(models.Model):
-    tierra = models.CharField(max_length=200, null=True)
+    tierra = models.CharField(max_length=200, null=True, blank=True)
     potasio = models.BooleanField(default=False)
     nitrogeno = models.BooleanField(default=False)
     fosforo = models.BooleanField(default=False)
-
 
     def __str__(self,):
         return self.tierra
 
 
 class Ficha(models.Model):
-    planta = models.ForeignKey(Planta, on_delete=models.SET_NULL, null=True, related_name='fichas')
-    
-    volumen_mazeta_ltr = models.DecimalField(null=False, max_digits=5, decimal_places=2)
-    profundidad_cm = models.DecimalField(null=False, max_digits=5, decimal_places=2)
-    tamano = models.CharField(max_length=200, null=False, choices=[
-        ('S','Chico'),
-        ('M','Mediano'),
-        ('L','Grande'),
-        ('XL','Extra Grande'),
-    ])
-    distancia = models.DecimalField(null=False, max_digits=5, decimal_places=2)
-    temperatura = models.DecimalField(null=False, max_digits=5,
-                                      decimal_places=2) # tiene que ser un rango
-    horas_sol = models.DecimalField(null=False, max_digits=5, decimal_places=2) # rango
-    riego = models.CharField(max_length=200, null=False, choices=[
-        ('c15D','Cada 15 días'),
-        ('1xS','Una vez por semana'),
-        ('2xS','Dos veces por semana'),
-        ('c2D','Cada dos días'),
-        ('1xD','Una vez por día'),
-        ('2xD','Dos veces por día'),
-    ])
-    tiempo_cultivo_semanas = models.IntegerField(null=False) # semanas
+    planta = models.ForeignKey(Planta, on_delete=models.SET_NULL, null=True,
+                               related_name='fichas')
+    tamano = models.CharField(max_length=200, null=True, blank=True,
+                              choices=[('S', 'Chico'),
+                                       ('M', 'Mediano'),
+                                       ('L', 'Grande'),
+                                       ('XL', 'Extra Grande')])
+    volumen_maceta_ltr = models.DecimalField(null=True, max_digits=5,
+                                             blank=True, decimal_places=0)
+    profundidad_cm = models.DecimalField(null=True, blank=True, max_digits=5,
+                                         decimal_places=0)
+    distancia_cm = models.DecimalField(null=True, blank=True, max_digits=5,
+                                       decimal_places=0)
+    temperatura = models.DecimalField(null=True, blank=True, max_digits=5,
+                                      decimal_places=0) # tiene que ser un rango
+    horas_sol = models.DecimalField(null=True, blank=True, max_digits=5,
+                                    decimal_places=0) # rango
     tutorado = models.BooleanField(default=False)
-    
-    epoca_semillero = models.ForeignKey(Epoca, on_delete=models.SET_NULL, null=True, related_name='fichas_semillero')
-    epoca_siembra = models.ForeignKey(Epoca, on_delete=models.SET_NULL, null=True, related_name='fichas_siembra')
-    epoca_trasplante = models.ForeignKey(Epoca, on_delete=models.SET_NULL, null=True, related_name='fichas_trasplante')
-    epoca_cosecha = models.ForeignKey(Epoca, on_delete=models.SET_NULL, null=True, related_name='fichas_cosecha')
-
-    sustrato = models.ManyToManyField(Sustrato)
-    tips = models.ManyToManyField(Tip)
-    fuentes = models.ManyToManyField(Fuente)
-
+    riego = models.CharField(max_length=200, null=True, blank=True, choices=[
+        ('c15D', 'Cada 15 días'),
+        ('1xS', 'Una vez por semana'),
+        ('2xS', 'Dos veces por semana'),
+        ('c2D', 'Cada dos días'),
+        ('1xD', 'Una vez por día'),
+        ('2xD', 'Dos veces por día'),
+    ])
+    tiempo_cultivo_semanas = models.IntegerField(null=True, blank=True)
+    epoca_semillero = models.ForeignKey(Epoca, on_delete=models.SET_NULL,
+                                        null=True, blank=True,
+                                        related_name='fichas_semillero')
+    epoca_siembra = models.ForeignKey(Epoca, on_delete=models.SET_NULL,
+                                      null=True, blank=True,
+                                      related_name='fichas_siembra')
+    epoca_trasplante = models.ForeignKey(Epoca, on_delete=models.SET_NULL,
+                                         null=True, blank=True,
+                                         related_name='fichas_trasplante')
+    epoca_cosecha = models.ForeignKey(Epoca, on_delete=models.SET_NULL,
+                                      null=True, blank=True,
+                                      related_name='fichas_cosecha')
+    sustrato = models.ManyToManyField(Sustrato, blank=True)
+    tips = models.ManyToManyField(Tip, blank=True)
+    fuentes = models.ManyToManyField(Fuente, blank=True)
 
     def __str__(self,):
-        return self.planta.nombre_popular if self.planta.nombre_popular else self.planta.nombre_cientifico
+        return get_name(self.planta)
 
 
 class Interacciones(models.Model):
     target = models.ForeignKey(Planta, on_delete=models.SET_NULL, null=True,
                                related_name='interaciones')
-    tipo = models.CharField(max_length=200, null=False, choices=[
-        ('B','Benéfica'),
-        ('P','Perjudicial'),
-    ])
-
-    actores = models.ManyToManyField(Planta)
-
+    # tipo = models.CharField(max_length=200, null=False, choices=[
+    #     ('B', 'Benéfica'),
+    #     ('P', 'Perjudicial'),
+    # ])
+    # actores = models.ManyToManyField(Planta, blank=True)
+    benefica = models.ManyToManyField(Planta, blank=True,
+                                      related_name='Benéfica')
+    perjudicial = models.ManyToManyField(Planta, blank=True,
+                                         related_name='Perjudicial')
 
     def __str__(self,):
-        return self.target.nombre_popular if self.target.nombre_popular else self.target.nombre_cientifico 
+        return get_name(self.target)
