4309d55a27e6b1b4a57466efec3c6f02eba69c0a
Ramiro Gatti <rgatti@ingenieria.uner.edu.ar>
2020-12-29 14:41:15 -0300

    estilo

--- rest/plantas/urls.py
+++ rest/plantas/urls.py
@@ -4,10 +4,8 @@ from plantas import views
 
 app_name = 'plantas'
 urlpatterns = [
-
     path('s', views.PlantaViewSet.as_view({'get': 'list', 'post': 'create'}),
          name='plantas-list'),
     path('<int:pk>', views.PlantaViewSet.as_view({'get': 'retrieve',
          'put': 'update', 'delete': 'destroy'}), name='plantas-list'),
-
 ]
--- rest/rest/utils.py
+++ rest/rest/utils.py
@@ -1,8 +1,9 @@
 import os
 
+
 def get_env_value(env_variable):
     try:
-      	return os.environ[env_variable]
+        return os.environ[env_variable]
     except KeyError:
         print('Set the {} environment variable'.format(env_variable))
         return None
