3dd344515abfb12067c60723191c8ee9de628116
Ramiro Gatti <rgatti@ingenieria.uner.edu.ar>
2020-12-29 13:33:57 -0300

    custom admin site

--- rest/plantas/admin.py
+++ rest/plantas/admin.py
@@ -1,14 +1,101 @@
 from django.contrib import admin
-
 from plantas import models
 
+FSFICHA = [(None, {'fields': [('planta')]}),
+           ('Dimensión', {'fields': [('tamano', 'volumen_maceta_ltr'),
+                          ('profundidad_cm', 'distancia_cm')]}),
+           ('Cuidados', {'fields': [('horas_sol_min', 'horas_sol_max',
+                         'sombra'), ('temperatura_min', 'temperatura_max'),
+                                    ('tutorado', 'riego', 'sustrato')]}),
+           ('Cultivo', {'fields': [('epoca_semillero', 'epoca_siembra'),
+                                   ('epoca_trasplante', 'epoca_cosecha'),
+                        'tiempo_cultivo_semanas']}),
+           (None, {'fields': [('tips', 'fuentes')]})]
+
+
+class InteraccionInline(admin.TabularInline):
+    model = models.Interaccion
+    extra = 0
+    can_delete = False
+    autocomplete_fields = ['target', 'benefica', 'perjudicial']
+
+
+class FichaInline(admin.StackedInline):
+    model = models.Ficha
+    extra = 0
+    fieldsets = FSFICHA
+    can_delete = False
+    autocomplete_fields = ['sustrato', 'tips', 'fuentes']
+
+
+class PlantaInline(admin.TabularInline):
+    model = models.Planta
+    extra = 0
+    can_delete = False
+
+
+class AutorInline(admin.TabularInline):
+    model = models.Autor
+    extra = 1
+    can_delete = False
+
+
+class PlantaAdmin(admin.ModelAdmin):
+    fields = (('nombre_popular', 'nombre_cientifico'),
+              ('variedad', 'familia', 'tipo'))
+    list_display = ('__str__', 'familia', 'tipo', 'variedad',
+                    'nombre_popular', 'nombre_cientifico')
+    list_editable = ('nombre_popular', 'nombre_cientifico',
+                     'variedad', 'familia', 'tipo')
+    list_filter = ['tipo', 'familia']
+    search_fields = ['nombre_popular', 'nombre_cientifico']
+    ordering = ['nombre_popular', 'variedad']
+    inlines = [InteraccionInline, FichaInline]
+
+
+    # def sortable_str(self, obj):
+    #     return obj.__str__()
+    # 
+    # sortable_str.short_description = 'Planta'
+    # sortable_str.admin_order_field = '__str__'
+
+
+class SustratoAdmin(admin.ModelAdmin):
+    search_fields = ['tierra']
+
+
+class AutorAdmin(admin.ModelAdmin):
+    search_fields = ['autor']
+
+
+class TipAdmin(admin.ModelAdmin):
+    search_fields = ['titulo']
+
+
+class FuenteAdmin(admin.ModelAdmin):
+    search_fields = ['primer_autor']
+    list_display = ('__str__', 'cita', 'url')
+    autocomplete_fields = ['primer_autor', 'otros_autores']
+
+
+class FichaAdmin(admin.ModelAdmin):
+    fieldsets = FSFICHA
+    list_display = ('__str__', 'tamano')
+    autocomplete_fields = ['sustrato', 'tips', 'fuentes']
+
+
+class InteraccionAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['target', 'benefica', 'perjudicial']
+
+
+admin.site.register(models.Planta, PlantaAdmin)
 admin.site.register(models.Familia)
-admin.site.register(models.Rotaciones)
+admin.site.register(models.Rotacion)
 admin.site.register(models.Epoca)
-admin.site.register(models.Fuente)
-admin.site.register(models.Tip)
-admin.site.register(models.Sustrato)
+admin.site.register(models.Autor, AutorAdmin)
+admin.site.register(models.Fuente, FuenteAdmin)
+admin.site.register(models.Tip, TipAdmin)
+admin.site.register(models.Sustrato, SustratoAdmin)
 admin.site.register(models.Tipo)
-admin.site.register(models.Planta)
-admin.site.register(models.Ficha)
-admin.site.register(models.Interacciones)
+admin.site.register(models.Ficha, FichaAdmin)
+admin.site.register(models.Interaccion, InteraccionAdmin)
