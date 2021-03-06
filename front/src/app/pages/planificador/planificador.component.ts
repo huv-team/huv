27e2ba9d9af131c9c22079efb4f5935225833f53
Guido Pascariello <guidop@kozaca.com.ar>
2021-03-06 17:45:01 -0300

    planificador al 50%

--- front/src/app/pages/planificador/planificador.component.ts
+++ front/src/app/pages/planificador/planificador.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { PlantasService } from 'src/app/services/plantas.service';
+
 @Component({
   selector: 'app-planificador',
   templateUrl: './planificador.component.html',
@@ -7,13 +9,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlanificadorComponent implements OnInit {
 
-  constructor() { }
+  query:any = {};
+  
+  volumenesMaceta:number[] = [];
+  profundidadesMaceta:number[] = [];
+  loading:boolean;
+  
+  constructor(
+    private plantasSrv:PlantasService
+  ) { }
 
   ngOnInit(): void {
+    this.loading = true;
+    this.plantasSrv.get_maseta_data().subscribe(
+      res => {
+        this.volumenesMaceta = res['volumenes'];
+        this.profundidadesMaceta = res['profundidades'];
+      },
+      err => {
+        console.log(err);
+      },
+      () => {
+        this.loading = false;
+      }
+    )
   }
 
-  print(val) {
-    console.log(val)
+  set actividad(data:string) {
+    this.query['actividad'] = data;
+  }
+  
+  set tipos(data:any) {
+    this.query['tipos'] = data;
+  }
+
+  set meses(data:any) {
+    this.query['meses'] = data;
+  }
+
+  set macetas(data:any){
+    this.query['macetas'] = data;
   }
 
 }
