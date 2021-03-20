3d2414a0b78b05b1f2f4ac0a9bf9931c73f5245c
Guido Pascariello <guidop@kozaca.com.ar>
2021-03-20 16:01:57 -0300

    eventos navegacion -- inicio

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+import { AppRouteChangeService } from "src/app/services/app-route-change.service";
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  
   title = 'front';
+
+  constructor (
+    private routeSrv:AppRouteChangeService
+  ) {}
 }
